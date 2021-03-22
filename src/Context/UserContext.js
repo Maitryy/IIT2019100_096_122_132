8e0214541d95f42500615ef9752ab264519a8493
Noob-Bot-1 <devbansal1512@gmail.com>
2021-03-22 22:05:43 +0530

    Difference of webpages in teacher and Student done

---
+++ src/Context/UserContext.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import React, { createContext, useState } from 'react'
+
+const userContext = createContext();
+
+function UserContextProvider(props) {
+
+    const [user,setUser] = useState();
+
+    async function getUser(){
+        const type = await axios.get("http://localhost:5000/auth/TypeOfUser");
+        //console.log(type);
+        setUser(type.data);
+    }
+    return (
+        <userContext.Provider value = {{user,getUser}}>
+            {props.children}
+        </userContext.Provider>
+    )
+}
+
+export default userContext;
+export {UserContextProvider};
