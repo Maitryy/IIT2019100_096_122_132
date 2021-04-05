d9cf0ee7fb07b2c6c76599dc4b4d1ed66d918ad0
maitryy <maitryjadiya123@gmail.com>
2021-04-05 17:26:55 +0530

    register page partial

--- src/Auth/Register.js
+++ src/Auth/Register.js
@@ -4,8 +4,10 @@ import React,{useState} from 'react';
 // import AuthContext from '../Context/AuthContext';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './RegisterStudent.css'
+import './Register.css'
 import StudentForm from './StudentForm';
 import TeacherForm from './TeacherForm';
+import image1 from '../images/register-head-01.png'
 
 function Register() {
     const [stud,setStudent] = useState(false);
@@ -17,19 +19,23 @@ function Register() {
 
     return (
         <div>
-            <h1>Register a new account for :</h1>
-            <button className = "student-btn" onClick = {handleStudent}>
+            <div className="regi">
+            <img className = "register-head" src={image1} alt=""/>
+            </div>
+            <div className = "container regi-btns">
+            <button className = "student-btn btn btn-lg btn-course" onClick = {handleStudent}>
                 Student
             </button>
                 <div className = {stud? 'create':'create-inactive'}>
                     <StudentForm />
                 </div>
-                <button className = "teacher-btn" onClick = {handleTeacher}>
+                <button className = "teacher-btn btn btn-lg btn-course" onClick = {handleTeacher}>
                 Teacher
             </button>
                 <div className = {teacher? 'create':'create-inactive'}>
                     <TeacherForm />
                 </div>
+            </div>
             
         </div>
     )
