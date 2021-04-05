dd8a8efa3c1586e57bf18900f42377626ea77fad
riyag0512 <riyag0512@gmail.com>
2021-04-05 09:38:13 +0530

    changed core coursw

--- src/Components/ExploreCourses/TechnicalElectiveCoursePage.js
+++ src/Components/ExploreCourses/TechnicalElectiveCoursePage.js
@@ -16,14 +16,27 @@ function TechnicalElectiveCoursePage() {
         <div>
              {data.map(course=>{
                 return(
+                    <div className= "col-lg-4 col-md-6 col-sm-12 col-12">
+                    <div key = '_id'>
+                        
+                        <div className='card courses mask '>
+                                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                    <a href="#" >
+                                        <img src="https://www.futureelectronics.com/medias/sys_master/images/images/9601962868766/CMSHEROShapingTheFuture1200x450-D.jpg" className="img-fluid card-img-top"/>
+                                    </a>
+                                </div>
+                                <div class="card-body">
                     <div key = '_id'>   
-                        <div>   Course name { course.name} </div>
-                        <div>   Course id { course.id} </div> 
-                        <div>   Course credits: { course.credits} </div>
-                        <div>   Course description { course.description } </div>
+                    <div>   <strong>Course name: </strong>{ course.name} </div>
+                        <div>  <strong>Course id: </strong>{ course.id} </div> 
+                        <div>   <strong>Course credits: </strong>{ course.credits} </div>
+                        <div>  <strong>Course description: </strong> { course.description } </div>
                         <br></br>
                     </div>
-                    
+                    </div>
+                        </div>
+                        </div>  
+                    </div>
                 )
             })}
         </div>
