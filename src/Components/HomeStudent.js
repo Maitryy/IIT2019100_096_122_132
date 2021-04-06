c8694b36f7126546e08fc9d0b5017a8e4622937b
riyag0512 <riyag0512@gmail.com>
2021-04-06 23:13:20 +0530

    updated homestudent

--- src/Components/HomeStudent.js
+++ src/Components/HomeStudent.js
@@ -1,14 +1,12 @@
-
-
-import React, { useContext , useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react'
 import '../bootstrap/bootstrap.css'
 import userContext from '../Context/UserContext'
 import './HomeStudent.css'
 
-
+import { Link } from 'react-router-dom';
 
 function HomeStudent() {
+    
     const [data,setData] = useState([]);
     const [data1,setData1] = useState([]);
     const [data2,setData2] = useState([]);
@@ -39,6 +37,9 @@ function HomeStudent() {
     useEffect( () => {
         console.log(data2);
     }, [data2]);
+
+
+
     const {userName} = useContext(userContext);
 
     return (
@@ -57,122 +58,220 @@ function HomeStudent() {
              </div>
 
 
-            {/* <h1>Enrolled Courses</h1> */}
+            {/* <h1>Enrolled Courses</h1> 
             <div className="card bg-dark text-white enroll-card">
                 <img className=" enroll-card-img" src="https://images.unsplash.com/photo-1579547621309-5e57ab324182?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" alt="Card image"/>
                 <div className="card-img-overlay">
                     <h1 className=" enroll-card-title card-title">Enrolled Courses</h1>
                     {/* <p className="enroll-card-text card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p className="card-text">Last updated 3 mins ago</p> */}
+                    <p className="card-text">Last updated 3 mins ago</p> 
                 </div>
             </div>
 
             <div className="All-courses row row-cols-3">
-            {data.map(course=>{
-                return(
-                    
-                    
-                    
-                    <div className= "col-lg-4 col-md-6 col-sm-12 col-12">
-                    <div key = '_id'>
-                        
-                        <div className='card courses mask '>
-                                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                                    <a href="#" >
-                                        <img src="https://www.futureelectronics.com/medias/sys_master/images/images/9601962868766/CMSHEROShapingTheFuture1200x450-D.jpg" className="img-fluid card-img-top"/>
-                                    </a>
-                                </div>
-                                <div class="card-body">
-                                       
-                                        <h4 className= " card-title">{ course.name}</h4>
-                                        {/* <h5 className = "card-subtitle text-muted">Amarnath Yadav</h5> */}
-                                        <p className="text-muted">Credits: { course.credits}</p>
-                                        <p className="text-muted">Mentors: { course.teacher}</p>
-                                       
-                                        <Link  to= {`/courseTeacher/${course._id}`} >
-                                            <button className="btn btn-primary btn-course"> Go to Course</button>
-                            
-                                        </Link>
-                                </div>
-                        </div>
-                        </div>  
-                    </div>
-                   
-                    
-                )
-            })}
 
-{data1.map(course=>{
-                return(
-                    
-                    
-                    
-                    <div className= "col-lg-4 col-md-6 col-sm-12 col-12">
-                    <div key = '_id'>
-                        
-                        <div className='card courses mask '>
-                                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                                    <a href="#" >
-                                        <img src="https://www.futureelectronics.com/medias/sys_master/images/images/9601962868766/CMSHEROShapingTheFuture1200x450-D.jpg" className="img-fluid card-img-top"/>
-                                    </a>
-                                </div>
-                                <div class="card-body">
-                                      
-                                        <h4 className= " card-title">{ course.name}</h4>
-                                        {/* <h5 className = "card-subtitle text-muted">Amarnath Yadav</h5> */}
-                                        <p className="text-muted">Credits: { course.credits}</p>
-                                        <p className="text-muted">Mentors: { course.teacher}</p>
-                                       
-                                        <Link  to= {`/courseTeacher/${course._id}`} >
-                                            <button className="btn btn-primary btn-course">Go to Course</button>
-                            
-                                        </Link>
-                                </div>
-                        </div>
-                        </div>  
-                    </div>
-                   
-                    
-                )
-            })}
+           <div className= "col-lg-4 col-md-6 col-sm-12 col-12">
+           <div className='card courses mask '>
+                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                    <a href="#" >
+                        <img src="https://images.unsplash.com/photo-1509869175650-a1d97972541a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" className="img-fluid card-img-top"/>
+                    </a>
+                </div>
+                <div class="card-body">
+                        <h4 className= " card-title">Maths</h4>
+                        <h5 className = "card-subtitle text-muted">Anand kumar tiwari</h5>
+                        <p className="text-muted">Credits:4</p>
+                        <p className="card-text"></p>
+                        <button className="btn btn-primary btn-course">Go to Course</button>
+                </div>
+           </div>
+           </div>
+           <div className= "col-lg-4 col-md-6 col-sm-12 col-12">
+           <div className='card courses mask '>
+                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                    <a href="#" >
+                        <img src="https://www.thoughtco.com/thmb/G9vU9zzR6Gffq9rpvJVeZ2YfO28=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/warning--data-transfer-in-progress-507065943-59c6d2a70d327a001141794d-5bb792f246e0fb0051a88f9c.jpg" className="img-fluid card-img-top" />
+                    </a>
+                </div>
+                <div class="card-body">
+                        <h4 className= "card-title">Introducion to Programming</h4>
+                        <h5 className = "card-subtitle text-muted">Mohammad Javed</h5>
+                        <p className="text-muted">Credits:4</p>
+                        <p className="card-text"></p>
+                        <button className="btn btn-primary btn-course">Go to Course</button>
+                </div>
+           </div>
+           </div>
+           <div className= "col-lg-4 col-md-6 col-sm-12 col-12">
+           <div className='card courses mask '>
+                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                    <a href="#" >
+                        <img src="https://www.futureelectronics.com/medias/sys_master/images/images/9601962868766/CMSHEROShapingTheFuture1200x450-D.jpg" className="img-fluid card-img-top"/>
+                    </a>
+                </div>
+                <div class="card-body">
+                        <h4 className= " card-title">Electronics</h4>
+                        <h5 className = "card-subtitle text-muted">Amarnath Yadav</h5>
+                        <p className="text-muted">Credits:4</p>
+                        <p className="card-text"></p>
+                        <button className="btn btn-primary btn-course">Go to Course</button>
+                </div>
+           </div>
+           </div>
+           <div className= "col-lg-4 col-md-6 col-sm-12 col-12">
+           <div className='card courses mask '>
+                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                    <a href="#" >
+                        <img src="https://images.unsplash.com/photo-1580894742597-87bc8789db3d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" className="img-fluid card-img-top"/>
+                    </a>
+                </div>
+                <div class="card-body">
+                        <h4 className= " card-title">Software Engineering</h4>
+                        <h5 className = "card-subtitle text-muted">Sonali </h5>
+                        <p className="text-muted">Credits: 4</p>
+                        <p className="card-text"></p>
+                        <button className="btn btn-primary btn-course">Go to Course</button>
+                </div>
+           </div>
+           </div>
+           <div className= "col-lg-4 col-md-6 col-sm-12 col-12">
+           <div className='card courses mask '>
+                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                    <a href="#" >
+                        <img src="https://images.unsplash.com/photo-1507146153580-69a1fe6d8aa1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" className="img-fluid card-img-top"/>
+                    </a>
+                </div>
+                <div class="card-body">
+                        <h4 className= " card-title">Machine Learning</h4>
+                        <h5 className = "card-subtitle text-muted">Rahul Kala</h5>
+                        <p className="text-muted">Credits:4</p>
+                        <p className="card-text"></p>
+                        <button className="btn btn-primary btn-course">Go to Course</button>
+                </div>
+           </div>
+           </div>
+           </div>
+            */}
 
-{data2.map(course=>{
-                return(
-                    
-                    
-                    
-                    <div className= "col-lg-4 col-md-6 col-sm-12 col-12">
-                    <div key = '_id'>
-                        
-                        <div className='card courses mask '>
-                                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                                    <a href="#" >
-                                        <img src="https://www.futureelectronics.com/medias/sys_master/images/images/9601962868766/CMSHEROShapingTheFuture1200x450-D.jpg" className="img-fluid card-img-top"/>
-                                    </a>
-                                </div>
-                                <div class="card-body">
-                                       
-                                        <h4 className= " card-title">{ course.name}</h4>
-                                        {/* <h5 className = "card-subtitle text-muted">Amarnath Yadav</h5> */}
-                                        <p className="text-muted">Credits: { course.credits}</p>
-                                        <p className="text-muted">Mentors: { course.teacher}</p>
-                                       
-                                        <Link  to= {`/courseTeacher/${course._id}`} >
-                                            <button className="btn btn-primary btn-course">Go to Course</button>
-                            
-                                        </Link>
-                                </div>
-                        </div>
-                        </div>  
-                    </div>
+     
+          
                    
-                    
-                )
-            })}
+                   <div className="card bg-dark text-white enroll-card">
+                       <img className=" enroll-card-img" src="https://images.unsplash.com/photo-1579547621309-5e57ab324182?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" alt="Card image"/>
+                       <div className="card-img-overlay">
+                           <h1 className=" enroll-card-title card-title">Enrolled Courses</h1>
+                       </div>
+                   </div>
+                   
+                   
+                   
+                   <div className="All-courses row row-cols-3">
+                   {data.map(course=>{
+                       return(
+                           
+                           
+                           
+                           <div className= "col-lg-4 col-md-6 col-sm-12 col-12">
+                           <div key = '_id'>
+                               
+                               <div className='card courses mask '>
+                                       <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                           <a href="#" >
+                                               <img src="https://www.futureelectronics.com/medias/sys_master/images/images/9601962868766/CMSHEROShapingTheFuture1200x450-D.jpg" className="img-fluid card-img-top"/>
+                                           </a>
+                                       </div>
+                                       <div class="card-body">
+                                              
+                                               <h4 className= " card-title">{ course.name}</h4>
+                                               {/* <h5 className = "card-subtitle text-muted">Amarnath Yadav</h5> */}
+                                               <p className="text-muted">Credits: { course.credits}</p>
+                                               <p className="text-muted">Mentors: { course.teacher}</p>
+                                              
+                                               <Link  to= {`/courseTeacher/${course._id}`} >
+                                                   <button className="btn btn-primary btn-course">Go to Course</button>
+                                   
+                                               </Link>
+                                       </div>
+                               </div>
+                               </div>  
+                           </div>
+                          
+                           
+                       )
+                   })}
+       
+       {data1.map(course=>{
+                       return(
+                           
+                           
+                           
+                           <div className= "col-lg-4 col-md-6 col-sm-12 col-12">
+                           <div key = '_id'>
+                               
+                               <div className='card courses mask '>
+                                       <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                           <a href="#" >
+                                               <img src="https://www.futureelectronics.com/medias/sys_master/images/images/9601962868766/CMSHEROShapingTheFuture1200x450-D.jpg" className="img-fluid card-img-top"/>
+                                           </a>
+                                       </div>
+                                       <div class="card-body">
+                                             
+                                               <h4 className= " card-title">{ course.name}</h4>
+                                               {/* <h5 className = "card-subtitle text-muted">Amarnath Yadav</h5> */}
+                                               <p className="text-muted">Credits: { course.credits}</p>
+                                               <p className="text-muted">Mentors: { course.teacher}</p>
+                                              
+                                               <Link  to= {`/courseTeacher/${course._id}`} >
+                                                   <button className="btn btn-primary btn-course">Go to Course</button>
+                                   
+                                               </Link>
+                                       </div>
+                               </div>
+                               </div>  
+                           </div>
+                          
+                           
+                       )
+                   })}
+       
+       {data2.map(course=>{
+                       return(
+                           
+                           
+                           
+                           <div className= "col-lg-4 col-md-6 col-sm-12 col-12">
+                           <div key = '_id'>
+                               
+                               <div className='card courses mask '>
+                                       <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                           <a href="#" >
+                                               <img src="https://www.futureelectronics.com/medias/sys_master/images/images/9601962868766/CMSHEROShapingTheFuture1200x450-D.jpg" className="img-fluid card-img-top"/>
+                                           </a>
+                                       </div>
+                                       <div class="card-body">
+                                              
+                                               <h4 className= " card-title">{ course.name}</h4>
+                                               {/* <h5 className = "card-subtitle text-muted">Amarnath Yadav</h5> */}
+                                               <p className="text-muted">Credits: { course.credits}</p>
+                                               <p className="text-muted">Mentors: { course.teacher}</p>
+                                              
+                                               <Link  to= {`/courseTeacher/${course._id}`} >
+                                                   <button className="btn btn-primary btn-course">Go to Course</button>
+                                   
+                                               </Link>
+                                       </div>
+                               </div>
+                               </div>  
+                           </div>
+                          
+                           
+                       )
+                   })}
+       
+                   </div>
+                  
+                  
 
-            </div>
-           
-        
            <div className="card bg-dark text-white enroll-card">
                 <img className=" enroll-card-img" src="https://images.unsplash.com/photo-1579547621309-5e57ab324182?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" alt="Card image"/>
                 <div className="card-img-overlay">
