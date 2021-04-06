4e282a7c0bf8662ba19a1a18276f53aed13aea0d
s-282 <yadav.sonal282@gmail.com>
2021-04-06 14:47:40 +0530

    added cards to my course page

--- src/Components/MyCourses.js
+++ src/Components/MyCourses.js
@@ -47,7 +47,24 @@ function MyCourses() {
         <div>
             
             <div className=' container' >
-            <h1>Core courses</h1>
+            {/* <h1>Core courses</h1> */}
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12 mt-3">
+                        <div className="card">
+                            <div className="card-horizontal">
+                                <div className="img-square-wrapper">
+                                    <img className="" src="https://images.unsplash.com/photo-1563729831178-d09061d83b12?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80" alt="Card image cap"/>
+                                </div>
+                                <div className="card-body">
+                                    <h4 className="card-title explore-title">Core Courses</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 {data.map(course=>{
     return(
           
@@ -83,7 +100,24 @@ function MyCourses() {
     </div>
 
     <div className=' container' >
-            <h1>Technical courses</h1>
+            {/* <h1>Technical courses</h1> */}
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12 mt-3">
+                        <div className="card">
+                            <div className="card-horizontal">
+                                <div className="card-body">
+                                    <h4 className="card-title explore-title">Technical Courses</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                                <div className="img-square-wrapper">
+                                    <img className="" src="https://images.unsplash.com/photo-1606337321936-02d1b1a4d5ef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80" alt="Card image cap"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 {data1.map(course=>{
     return(
           
@@ -120,7 +154,25 @@ function MyCourses() {
 
     
     <div className=' container' >
-            <h1>Non-Technical courses</h1>
+            {/* <h1>Non-Technical courses</h1> */}
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12 mt-3">
+                        <div className="card">
+                            <div className="card-horizontal">
+                                <div className="img-square-wrapper">
+                                    <img className="" src="https://images.unsplash.com/photo-1589254066213-a0c9dc853511?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"/>
+                                </div>
+                                <div className="card-body">
+                                    <h4 className="card-title explore-title">Non-Technical  Courses</h4>
+                                    <p className="card-text"> Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
 {data2.map(course=>{
     return(
           
