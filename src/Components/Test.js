bd0a43d766f401dca8a420bbc9b917064349327f
riyag0512 <riyag0512@gmail.com>
2021-04-11 16:18:03 +0530

    test.js

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -4,33 +4,42 @@ import React, {useState, useContext } from 'react'
 import { useHistory } from 'react-router';
 import userContext from '../Context/UserContext'
 
-function Faq(props) {
-    const [question,setQuestion] = useState();
-    
+function Test(props) {
+    const [test,setTest] = useState();
+    const [questionNum,setQuestionNum] = useState();
+    const [dueTime,setdueTime] = useState();
   
+    const [question,setQuestion1] = useState();
+  
+    const [xyz,setXyz]= useState(false);
+  
+ const[testId, setTestId]= useState();
+
     const history = useHistory();
     const {userName} = useContext(userContext);
    
-     async function createFaq(e){
+     async function createTest(e){
          e.preventDefault();
+
 
         try {
             const course_id = props.match.params.id ;
            
 
-            console.log(course_id);
-            console.log(userName);
-            const student=userName;
-
             const newFaq = {
                 course_id,
+                testName,
                 question,
-              student
+                dueTime
+             
             };
 
-            await axios.post("http://localhost:5000/course/Faq", newFaq);
-             history.push("/HomeStudent");
-            
+            const test_id=await axios.post("http://localhost:5000/test/NewTest",newFaq );
+          
+            setTestId(test_id);
+            setXyz(true);
+
+
         } catch (err) {
          console.error(err);
          }
@@ -40,19 +49,46 @@ function Faq(props) {
         <div>
     <h1>heyyyyyy</h1>
              <form className = "create"
-                        onSubmit = {createFaq}
+                        onSubmit = {createTest}
                     >
                         <div className="prop">
-                            <label >Question</label>
+                            <label >Test Name</label>
                         <input 
                             type='string' 
-                            placeholder = 'question' 
-                            onChange = {(e) => setQuestion(e.target.value)}
+                            placeholder = 'test name' 
+                            onChange = {(e) => setTest(e.target.value)}
+                            value= {testName}
+                        />
+                         </div>
+                      
+                        
+                        <div className="prop">
+                            <label >Question 1</label>
+                        <input 
+                            type='string' 
+                            placeholder = 'question 1' 
+                            onChange = {(e) => setQuestion1(e.target.value)}
                             value= {question}
                         />
                         </div>
 
-                  
+                           
+                     
+               
+                        <div className="prop">
+                            <label >Due time</label>
+                        <input 
+                            type='string' 
+                            placeholder = 'due time (hr:min::dd/mm/yyyy)' 
+                        
+                            onChange = {(e) => setdueTime(e.target.value)}
+                            value= {dueTime}
+                        />
+                        </div>
+
+                        
+                       
+
                                        
                         <button className="btn btn-course" type = 'submit'>Submit</button>
                     </form>       
