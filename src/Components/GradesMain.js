3e083d3feedbaa51dbe084d8d48cda0aecbc264f
maitryy <maitryjadiya123@gmail.com>
2021-05-01 20:45:17 +0530

    student grades view

--- src/Components/GradesMain.js
+++ src/Components/GradesMain.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import userContext from '../Context/UserContext';
+import './GradesMain.css'
 
 function GradesMain() {
     const [data,setData] = useState([]);
@@ -20,12 +21,34 @@ function GradesMain() {
                 <>
                 {data.arr.map(co => {
                     return(
-                        <>
-                            <div>
+                        <div className="container">
+                            {/* <div>
                                 {co.name} 
                                 <Link to = {`/GradesCourse/${co.id}`}>Click here</Link>
+                                
+                            </div> */}
+                            <div className="row">
+                            <div className="col-12 mt-3">
+                                <div className="card card-res">
+                                    <div className="card-horizontal card-announcements">
+                                    <div className="img-square-wrapper">
+                                            <img className="" src={co.image} alt="Card image cap"/>
+                                            
+                                        </div>
+                                        <div className="card-body">
+                                            <h4 className="card-title explore-title">{co.name}</h4>
+                                            <p className="card-text">To see your Performance in this course</p>
+                                            {/* <a className="card-text text-muted" href=" {announcement.link}">
+                                            {announcement.link}
+                                            </a> */}
+                                            <Link to = {`/GradesCourse/${co.id}`}><button className="btn btn-course btn-res ">Click here</button></Link>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
                             </div>
-                        </>
+                        </div>
+                        </div>
                 )})}
                 </>
             }
