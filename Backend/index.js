2c1b87dd1daea2f30f488209823aba7ea1f3f807
Noob-Bot-1 <devbansal1512@gmail.com>
2021-05-05 17:02:41 +0530

    Changes

--- Backend/index.js
+++ Backend/index.js
@@ -4,26 +4,64 @@ const app = express();
 const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+const crypto = require('crypto');
+
 
 const PORT = 5000;
 // app.use();
 app.use(express.json());
+//app.use(methodOverride('_method'));
 app.use(cookieParser());
 app.use(cors({
     origin: ["http://localhost:3000"],
     credentials: true
 }));
 
-mongoose.connect(process.env.MDB_CONNECT, {
+const conn = mongoose.connect(process.env.MDB_CONNECT, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+});
+
+// let gfs;
+
+// conn.once('open', () => {
+//   // Init stream
+//   gfs = Grid(conn.db, mongoose.mongo);
+//   gfs.collection('uploads');
+// });
+
+// const storage = new GridFsStorage({
+//     url: process.env.MDB_CONNECT,
+//     file: (req, file) => {
+//       return new Promise((resolve, reject) => {
+//         crypto.randomBytes(16, (err, buf) => {
+//           if (err) {
+//             return reject(err);
+//           }
+//           const filename = buf.toString('hex') + path.extname(file.originalname);
+//           const fileInfo = {
+//             filename: filename,
+//             bucketName: 'uploads'
+//           };
+//           resolve(fileInfo);
+//         });
+//       });
+//     }
+//   });
+//   const upload = multer({ storage });
+
+// app.post('/uploadFile', upload.single('file'), (req, res) => {
+//     // kres.json({ file: req.file });
+//     res.redirect('/');
+// });
 
 
 app.use("/auth", require("./routers/UserRouter"));
-
 app.use("/course", require("./routers/CourseRouter"));
-
 app.use("/test", require("./routers/TestRouter"));
 
 app.listen(PORT, () => console.log('Server started on port:'+ PORT));
--- Backend/models/assignmentModel.js
+++ Backend/models/assignmentModel.js
@@ -4,7 +4,7 @@ const assignmentSchema = new mongoose.Schema({
     course: {type: String, required: true},
     description: {type: String, required: true},
     date: {type: String, required: true},
-    pdf: Schema.Types.Mixed        
+    pdf: {type: String, required: true}     
 });
 
 const Assignment = mongoose.model("Assignment", assignmentSchema);
