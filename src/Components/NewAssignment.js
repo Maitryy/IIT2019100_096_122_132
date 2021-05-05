b002d7b1b881cf14f66ef9b3c7e966de9e2797c2
Noob-Bot-1 <devbansal1512@gmail.com>
2021-05-05 17:03:28 +0530

    2klend

--- src/Components/NewAssignment.js
+++ src/Components/NewAssignment.js
@@ -1,33 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 function NewAssignment() {
 
-    async function uploadAction(e) {
-        const data = new FormData();
-        const imagedata = e.target.files[0];
-        data.append('inputname', imagedata);
-    
-        await fetch("http://localhost:5000/uploadFile", {
-            mode: 'no-cors',
-            method: "POST",
-            body: data
-          }).then(function (res) {
-            if (res.ok) {
-              alert("Perfect! ");
-            } else if (res.status == 401) {
-              alert("Oops! ");
-            }
-          }, function (e) {
-            alert("Error submitting form!");
-          });
-        console.log(data.get('inputname')); 
-      }
     return (
         <div>
-            <form encType="multipart/form-data" action="">
-                <input type="file" name="fileName" defaultValue="fileName"></input>
-                <button type="submit" value="upload" onClick={this.uploadAction.bind(this)}></button>
-            </form>
+            
             
         </div>
     )
